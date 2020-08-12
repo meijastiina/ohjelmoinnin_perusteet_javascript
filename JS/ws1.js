0593fa727222f25e87b0490d7273b3560dcace5b
Meija Lohiniva <meija.lohiniva@seamk.fi>
2020-08-12 14:05:47 +0300

    Tehtävä 1.4 lisätty

--- JS/ws1.js
+++ JS/ws1.js
@@ -1,8 +1,11 @@
 /* 1. document.write - Ensimmäinen tehtäväsi on klassinen Hello world, jonka ideana on saada kirjoitettua sivulle jotakin JavaScriptin avulla.*/
-document.write("1. Hello world!");
+document.write("Tehtävä 1. Hello world!");
 
 /* 2. alert - Kokeillaan seuraavaksi tervehtimistä vähän eri tavalla, viestiboksin kautta. */
-alert('2. Hello world!');
+alert('Tehtävä 2. Hello world!');
 
 /* 3. document.getElementById - Viestin kirjoittaminen haluttuun elementtiin sivulla.*/
-document.getElementById('write-here').innerHTML = '3. Hello world!';
+document.getElementById('write-here').innerHTML = 'Tehtävä 3. Hello world!';
+
+/* 4. Lukeminen */
+document.getElementById('fullname').value = 'Tehtävä 4.' + document.getElementById('firstname').value + ' ' + document.getElementById('lastname').value;
