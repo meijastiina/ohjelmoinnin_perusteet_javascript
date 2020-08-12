b81af57f061481ead24a884c2f5f03d99377dd62
Meija Lohiniva <meija.lohiniva@seamk.fi>
2020-08-12 11:08:53 +0300

    Lisätty 1.3, muokattu otsikoita

--- JS/ws1.js
+++ JS/ws1.js
@@ -1,1 +1,8 @@
-document.write("Hello world!")
+/* 1. document.write - Ensimmäinen tehtäväsi on klassinen Hello world, jonka ideana on saada kirjoitettua sivulle jotakin JavaScriptin avulla.*/
+document.write("1. Hello world!");
+
+/* 2. alert - Kokeillaan seuraavaksi tervehtimistä vähän eri tavalla, viestiboksin kautta. */
+alert('2. Hello world!');
+
+/* 3. document.getElementById - Viestin kirjoittaminen haluttuun elementtiin sivulla.*/
+document.getElementById('write-here').innerHTML = '3. Hello world!';
